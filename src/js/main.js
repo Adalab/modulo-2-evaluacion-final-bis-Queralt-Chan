6aac65e27a771139f3050f1292ba5c4e7f6bb9c3
queralt-chan <queraltbali@gmail.com>
2024-01-29 12:04:36 +0800

    apiresults

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,20 @@ const btnS = document.querySelector ('.js-btnSave');
 const btnR = document.querySelector ('.js-btnRecover');
 const friendsList = document.querySelector ('.js-friendsList');
 
+let arrayFriends = [];
+function getDataApi (){
 fetch ('https://randomuser.me/api/?results=10')
 .then ((response) => response.json())
 .then ((data) => {
-    console.log (data)
+    console.log (data);
+    arrayFriends = data.results;
+    for (const urlList of arrayFriends) {
+        friendsList.innerHTML+= `<li> <img src="${urlList}" alt="friends">
+    </li>`
+    
+    }
 });
+
+}
+
+getDataApi ();
