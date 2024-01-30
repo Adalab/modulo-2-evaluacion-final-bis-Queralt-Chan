1d0e36910e029abc7843116f5a98f875fb8a99ab
queralt-chan <queraltbali@gmail.com>
2024-01-30 15:19:21 +0800

    addevent

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,25 @@ const btnS = document.querySelector ('.js-btnSave');
 const btnR = document.querySelector ('.js-btnRecover');
 const friendsList = document.querySelector ('.js-friendsList');
 
+
 let arrayFriends = [];
+
+
+function handleClick(event) {
+    const clickedCard = event.currentTarget.id;
+    console.log(clickedCard);
+    
+}
+function listenerFriends () {
+    const allFriends = document.querySelectorAll ('.js-amiguiContainer');
+
+    for (const oneFriend of allFriends) {
+       oneFriend.addEventListener ('click, handleClick');
+    }
+    
+}
+listenerFriends ();
+
 function getDataApi (){
 fetch ('https://randomuser.me/api/?results=10')
 .then ((response) => response.json())
@@ -14,15 +32,17 @@ fetch ('https://randomuser.me/api/?results=10')
     
     console.log (arrayFriends);
     for (const friendData of arrayFriends) {
-        friendsList.innerHTML+= `<li class="amiguiContainer"> <img class="imageFriend" src="${friendData.picture.medium}" alt="friends">
+        friendsList.innerHTML+= `<li class="js-amiguiContainer" id="${friendData.id}"> <img class="imageFriend" src="${friendData.picture.medium}" alt="friends">
         <h3>${friendData.name.title} ${friendData.name.first} ${friendData.name.last}<h3> 
         <h3>${friendData.location.city}<h3> 
         <h4>${friendData.login.username}<h4>
     </li>`
-    
     }
-});
 
-}
-
+     }
+      
+     
+)}; 
+// esta linia cierra el fetch
 getDataApi ();
+
