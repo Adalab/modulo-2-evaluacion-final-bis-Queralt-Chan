78d5d1afa6dd4a0aa7017873635ade5ca7b6d0bb
queralt-chan <queraltbali@gmail.com>
2024-02-01 16:21:57 +0800

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
-document.querySelector(".js-btnSave");document.querySelector(".js-btnRecover");const r=document.querySelector(".js-friendsList");let t=[friends].isFriend=!0;function o(n){const e=n.currentTarget.id,i=t.findIndex(s=>s.login.uuid===e);console.log(i),i===!0&&e.classList.add("card__selected")}function c(){const n=document.querySelectorAll(".js-amiguiContainer");for(const e of n)e.addEventListener("click",o)}function l(){fetch("https://randomuser.me/api/?results=10").then(n=>n.json()).then(n=>{console.log(n.results),t=n.results,localStorage.setItem("users",JSON.stringify(t)),console.log(t);for(const e of t)r.innerHTML+=`<li class="js-amiguiContainer card__selected" id="${e.login.uuid}"><img class="imageFriend" src="${e.picture.medium}" alt="friends">
+document.querySelector(".js-btnSave");document.querySelector(".js-btnRecover");const c=document.querySelector(".js-friendsList");let i=[];function t(n){const e=n.currentTarget.id,s=i.findIndex(r=>r.login.uuid===e);console.log(s)}findFriends!==-1&&(i[findFriends].isFriend=!0,clickedCard.classList.add("card__selected"));t();function o(){const n=document.querySelectorAll(".js-amiguiContainer");for(const e of n)e.addEventListener("click",t)}function d(){fetch("https://randomuser.me/api/?results=10").then(n=>n.json()).then(n=>{console.log(n.results),i=n.results,console.log(i);for(const e of i)c.innerHTML+=`<li class="js-amiguiContainer card__selected" id="${e.login.uuid}"><img class="imageFriend" src="${e.picture.medium}" alt="friends">
         <h3>${e.name.title} ${e.name.first} ${e.name.last}</h3> 
         <h3>${e.location.city}</h3> 
         <h4>${e.login.username}</h4> 
-    </li>`;c()})}l();
+    </li>`;o()})}d();
 //# sourceMappingURL=main.js.map
